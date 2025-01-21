ff7b8f4b5cff7a5f245a527f007afaceaa0a8470
Suraj Jha <sjha5505@gmail.com>
2025-01-21 21:26:54 +0530

    --changes in vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      "ys84sh-5173.csb.app", // Add the specific host here
+      "localhost", // Allow localhost (for local dev)
+      "0.0.0.0", // Allow all IP addresses
+    ],
+  },
+});
